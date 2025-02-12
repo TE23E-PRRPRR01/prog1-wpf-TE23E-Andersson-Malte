2a7329f340e4d84ee18281501307f74856f74872
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2025-02-12 15:03:11 +0100

    tränat på xaml o ch tryparse

---
+++ SlumpaApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlumpaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickSlumpa(object sender, RoutedEventArgs e)
+    {
+        //Kolla om värdet är giltigt
+        if (int.TryParse(Maxvärde.Text, out int maxvärdet))
+        {
+            //Giltigt, då använd användarens maxvärde
+            maxvärdet = int.Parse(Maxvärde.Text);
+            Felmeddelande.Text = "";
+        }
+        else
+        {
+            //Inte giltigt, sätt maxvärde till 100 och skicka felmeddelande
+            maxvärdet = 100;
+            Felmeddelande.Text = "Ogiltigt heltal. Maxvärdet sattes till 100";
+        }
+        
+        //int maxvärdet = int.Parse(Maxvärde.Text);
+
+        //Slumpa tal
+        int slumptal = Random.Shared.Next(1, maxvärdet+1);
+
+        //Skriva ut
+        Slumptalet.Text = slumptal.ToString();
+    }
+}
