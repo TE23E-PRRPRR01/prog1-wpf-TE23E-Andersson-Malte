d208f8e62067b98ffa4bbead2df83ec90e9fb5c5
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2025-02-17 13:21:30 +0100

    tränat på click-funktioner och sånt

---
+++ MelloApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MelloApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    int antalRöd = 0;
+    int antalGrön = 0;
+    int antalBlå = 0;
+    int antalGul = 0;
+    int antalLila = 0;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickRösta(object sender, RoutedEventArgs e)
+    {
+        //För varje klick + 1
+        //Vem klickade?
+        if (sender == Röd)
+        {
+            antalRöd++;
+        }
+        else if (sender == Grön)
+        {
+            antalGrön++;
+        }
+        else if (sender == Blå)
+        {
+            antalBlå++;
+        }
+        else if (sender == Gul)
+        {
+            antalGul++;
+        }
+        else if (sender == Lila)
+        {
+            antalLila++;
+        }
+
+        txbResultat.Text = $"Röd: {antalRöd} Grön: {antalGrön} Blå: {antalBlå} Gul: {antalGul} Lila: {antalLila}";
+    }
+
+    private void KlickRensa(object sender, RoutedEventArgs e)
+    {
+        int antalRöd = 0;
+        int antalGrön = 0;
+        int antalBlå = 0;
+        int antalGul = 0;
+        int antalLila = 0;
+
+        txbResultat.Text = $"Röd: {antalRöd} Grön: {antalGrön} Blå: {antalBlå} Gul: {antalGul} Lila: {antalLila}";
+    }
+}
